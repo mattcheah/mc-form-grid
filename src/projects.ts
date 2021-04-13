e3dba6ec189179e73ab375516a8398cc924c440c
mattcheah <matt.cheah@gmail.com>
2021-04-13 15:37:27 -0700

    things working much better than last time

--- src/projects.ts
+++ src/projects.ts
@@ -2,6 +2,8 @@
  * Public API Surface of mc-form-grid
  */
 
-export * from './lib/mc-form-grid.service';
-export * from './lib/mc-form-grid.component';
+export * from './lib/form-grid.component';
+export * from './lib/form-input/form-input.component';
+export * from './lib/form-divider/form-divider.component';
+export * from './lib/form-custom/form-custom.component';
 export * from './lib/mc-form-grid.module';
