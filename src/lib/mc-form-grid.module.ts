01d8da20b50017b63fe6f8fedc5291583a608786
Matt Cheah <matt.cheah@gmail.com>
2022-04-05 15:29:35 -0700

    Revert "allow input groups to still display form control errors" What idiot made this stupid commit? This reverts commit 415e2ac1f06fc58e7ca15674a24ebe9292daa727.

--- src/lib/mc-form-grid.module.ts
+++ src/lib/mc-form-grid.module.ts
@@ -1,31 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormCustomComponent } from './form-custom/form-custom.component';
 import { FormDividerComponent } from './form-divider/form-divider.component';
 import { FormGridComponent } from './form-grid.component';
 import { FormInputComponent } from './form-input/form-input.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {
-  COLOR_OPTIONS,
-  defaultColorOptions,
-  defaultFormCustomOptions,
-  defaultFormGridOptions,
-  defaultFormInputOptions,
-  FORM_CUSTOM_OPTIONS,
-  FORM_GRID_OPTIONS,
-  FORM_INPUT_OPTIONS,
-} from './optionsConfig';
+import { COLOR_OPTIONS, defaultColorOptions, defaultFormCustomOptions, defaultFormGridOptions, defaultFormInputOptions, FORM_CUSTOM_OPTIONS, FORM_GRID_OPTIONS, FORM_INPUT_OPTIONS } from './optionsConfig';
 
 @NgModule({
   declarations: [FormGridComponent, FormInputComponent, FormCustomComponent, FormDividerComponent],
   imports: [CommonModule, ReactiveFormsModule, NgbModule],
   exports: [FormGridComponent, FormInputComponent, FormCustomComponent, FormDividerComponent],
   providers: [
-    { provide: FORM_GRID_OPTIONS, useValue: defaultFormGridOptions },
-    { provide: FORM_INPUT_OPTIONS, useValue: defaultFormInputOptions },
-    { provide: FORM_CUSTOM_OPTIONS, useValue: defaultFormCustomOptions },
-    { provide: COLOR_OPTIONS, useValue: defaultColorOptions },
-  ],
+    {provide: FORM_GRID_OPTIONS, useValue: defaultFormGridOptions},
+    {provide: FORM_INPUT_OPTIONS, useValue: defaultFormInputOptions},
+    {provide: FORM_CUSTOM_OPTIONS, useValue: defaultFormCustomOptions},
+    {provide: COLOR_OPTIONS, useValue: defaultColorOptions}
+  ]
 })
-export class McFormGridModule {}
+export class McFormGridModule { }
